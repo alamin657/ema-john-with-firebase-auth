49204fbc99a9b7d733e49ecd98d2bcc150a9953b
Alamin <alamin017514@gmail.com>
2023-04-21 16:15:49 +0600

    ema john with firebase website done

--- src/_Steps_.js
+++ src/_Steps_.js
@@ -6,3 +6,32 @@
  * 5. Build> Authentication > get started > enable sign in method
  * 6. create sign up and Login route
  */
+/**
+ * 1.Create a context provider file
+ * 2. Create a context and set provider
+ * 3. set the children props
+ * 4. set context value
+ * 5. set the provider
+ */
+
+/**
+ * Build>Hosting--------
+ * --------------HOSTING------------
+ * --------------One time per PC--------
+ * 1. npm install -g firebase-tools
+ * 2. firebase login
+ * 
+ * For each project one time
+ * 1. firebase init 
+ * 2. procees
+ * 3. Hosting: firebase(up and down arrow) use space bar to select
+ * 4. existintg project
+ * 5. select the project careful
+ * 6. which project as public directory: dist
+ * 7. single page application:yes
+ * 8. continuous deployment: no
+ * ---------For every time deploy----------
+ * 1. npm run build 
+ * 2. firebase deploy
+ * 
+ */

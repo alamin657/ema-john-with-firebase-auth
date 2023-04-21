12cfb3c2c28dfcbdb03ecfeb61212e3babcd40e0
Alamin <alamin017514@gmail.com>
2023-04-21 08:43:20 +0600

    Login css and sign up css completed

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBR9iKpYD2eUDnZHBKBwm3Hn1xHE8TVj9Y",
+    authDomain: "ema-john-with-firebase-a-85580.firebaseapp.com",
+    projectId: "ema-john-with-firebase-a-85580",
+    storageBucket: "ema-john-with-firebase-a-85580.appspot.com",
+    messagingSenderId: "710007054246",
+    appId: "1:710007054246:web:7a19832e38e56f48153967"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
